e1fe9694247bc36b4e389801cc278d2396bc63da
bipt-lyle <2645385939@qq.com>
2024-01-21 21:25:51 +0100

    test

--- app.py
+++ app.py
@@ -1,9 +1,31 @@
-from flask import Flask, request
+from flask import Flask, request, render_template, redirect, url_for
 import subprocess
 
 app = Flask(__name__)
 
+items = []
+@app.route('/')
+def index():
+    return render_template('index.html', items=items)
 
+@app.route('/add', methods=['POST'])
+def add_item():
+    item = request.form.get('item')
+    if item:
+        items.append(item)
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:index>')
+def delete_item(index):
+    if index < len(items):
+        items.pop(index)
+    return redirect(url_for('index'))
+
+@app.route('/update/<int:index>', methods=['POST'])
+def update_item(index):
+    if index < len(items):
+        items[index] = request.form.get('new_item')
+    return redirect(url_for('index'))
 
 @app.route('/testing', methods=['POST'])
 def testing():
